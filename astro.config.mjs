df777d5dbd883d38dd4e13b9c2628fea66aa8d4e
william turner <willguyturner@gmail.com>
2025-04-14 20:51:01 +1200

    fix astro config base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://williamgturner.github.io",
+  base: "/myblog/",
   devToolbar: {
     enabled: false,
   },

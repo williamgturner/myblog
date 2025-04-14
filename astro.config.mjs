96955044d0137ba6823ed36f5590d47e6ec02ab8
william turner <willguyturner@gmail.com>
2025-04-14 20:39:18 +1200

    update astro config
    Update config to allow hosting on github pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://williamgturner.github.io",
+  base: "/myblog",
   devToolbar: {
     enabled: false,
   },

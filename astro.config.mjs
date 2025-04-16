aba87e20ce56a5a74b322d9fbc284a01d5998cf6
william turner <willguyturner@gmail.com>
2025-04-16 12:39:25 +1200

    netlify bugfixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://williamgturner.github.io",
-  base: "/myblog/",
+  site: "https://willturner.netlify.app/",
   devToolbar: {
     enabled: false,
   },

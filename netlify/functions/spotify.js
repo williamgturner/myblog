457df1213270806e91d25f1965a232b4110f5a2f
william turner <willguyturner@gmail.com>
2025-04-16 16:05:08 +1200

    fix unwanted scroll on main layout

--- netlify/functions/spotify.js
+++ netlify/functions/spotify.js
@@ -29,9 +29,9 @@ async function getAccessToken() {
   return data.access_token;
 }
 
-async function getTopTrack(accessToken) {
+async function getTopTracks(accessToken) {
   const res = await fetch(
-    "https://api.spotify.com/v1/me/top/tracks?limit=1&time_range=medium_term",
+    "https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=short_term",
     {
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -44,35 +44,33 @@ async function getTopTrack(accessToken) {
   }
 
   const data = await res.json();
-  const topTrack = data.items?.[0];
-
-  return {
-    title: topTrack?.name,
-    artist: topTrack?.artists?.map((artist) => artist.name).join(", "),
-    album: topTrack?.album?.name,
-    albumImageUrl: topTrack?.album?.images?.[0]?.url,
-    songUrl: topTrack?.external_urls?.spotify,
-  };
+  return data.items.map((track) => ({
+    title: track.name,
+    artist: track.artists.map((artist) => artist.name).join(", "),
+    album: track.album.name,
+    albumImageUrl: track.album.images?.[0]?.url,
+    songUrl: track.external_urls.spotify,
+  }));
 }
 
 exports.handler = async function () {
   try {
     const accessToken = await getAccessToken();
-    const topTrack = await getTopTrack(accessToken);
+    const topTracks = await getTopTracks(accessToken);
 
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(topTrack),
+      body: JSON.stringify(topTracks),
     };
   } catch (err) {
     console.error("Spotify fetch error:", err);
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: "Failed to fetch top track",
+        error: "Failed to fetch top tracks",
         details: err.message,
       }),
     };

e828fc9078333b54eaca25f2e8cf72171cefc854
william turner <willguyturner@gmail.com>
2025-04-17 12:49:14 +1200

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: "server", // this is required for SSR and functions
+  site: "https://willturner.netlify.app",
   adapter: netlify(),
   experimental: {
     session: true,
